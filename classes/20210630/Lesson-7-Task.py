25435a5a7aa1fcfbb079adc84b664c68daf4f596
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-07-03 16:01:54 -0500

    homework

--- classes/20210630/Lesson-7-Task.py
+++ classes/20210630/Lesson-7-Task.py
@@ -1,3 +1,8 @@
+import string
+
+
+# Practice 7.1
+
 def string_length(value):
     return len(value)
 
@@ -9,6 +14,8 @@ def practice_7_1():
     print(f"The length of '{value}' is {length}")
 
 
+# Practice 7.2
+
 def character_frequency(value):
     result = {}
     for character in value:
@@ -19,13 +26,48 @@ def character_frequency(value):
 def practice_7_2():
     print("=========================\nPractice 7.2\n-------------------------")
     value = 'String to calculate character frequency'
+    print(value)
     result = character_frequency(value)
     print(result)
 
+# Practice 7.3
 
 def practice_7_3():
     print("=========================\nPractice 7.3\n-------------------------")
+    fmt = string.Formatter()
+    custom_string = "The items are {}, {} and {}"
+    print("string.Formatter.format:\n - ", fmt.format(custom_string, 1, 2, 3))
+    custom_string = "lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    line_jump = '\n'
+    print(f"Original string:   {custom_string:>70}")
+    print(f"string.capitalize: {custom_string.capitalize():>71}")
+    print(f"string.casefold:   {custom_string.casefold():>70}")
+    print(f"string.center:     {custom_string.center(70, '-')}")
+    print(f"string.ljust:     {custom_string.ljust(70, '-')}")
+    print(f"string.rjust:     {custom_string.rjust(70, '-')}")
+    print(f"string.replace:     {custom_string.replace('ipsum', 'IPSUM'):>70}")
+    print(f"string.rpartition:     {custom_string.rpartition('ipsum')}")
+    print(f"string.splitlines:     {custom_string.replace(',', line_jump).splitlines()}")
+
+
+def celsius_to_fahrenheit(celsius_input):
+    return celsius_input * 9 / 5 + 32
+
+
+def fahrenheit_to_celsius(fahrenheit_input):
+    return (fahrenheit_input - 32) * 5 / 9
+
+
+def practice_7_4():
+    print("=========================\nPractice 7.4\n-------------------------")
+    for fahrenheit in [0, 32, 46.4, 212]:
+        print(f"- {fahrenheit} Fahrenheit equals {fahrenheit_to_celsius(fahrenheit):.2f} Celsius")
+    print("-------------------------")
+    for celsius in [-17.77778, 0, 8, 100]:
+        print(f"- {celsius} Celsius equals {celsius_to_fahrenheit(celsius):.2f} Fahrenheit")
 
 
 practice_7_1()
 practice_7_2()
+practice_7_3()
+practice_7_4()
--- classes/Assignment-1/c0816681_assignment.py
+++ classes/Assignment-1/c0816681_assignment.py
@@ -1,59 +1,39 @@
 # Part 1) The simplest encryption algorithm!
 
-# Provides the sets of alphabetical characters to be used, defined by the first and last code
-character_sets = [
-    (ord('A'), ord('Z')),
-    (ord('a'), ord('z')),
-]
-
-
-# Provides the encryption of a single character with a given set of characters and delta value
-# Returns two values
-# - A boolean value indicating if the character is part of the character set defined by case_range.
-# - The encrypted character
-def caesar_cypher_character(character_code, case_range, delta):
-    if character_code < case_range[0] or character_code > case_range[1]:
-        return False, None
-    position = character_code - case_range[0]
-    new_position = (position - delta) % (case_range[1] - case_range[0] + 1)
-    return True, chr(case_range[0] + new_position)
-
-
-# Provides the encryption of an entire string value using a delta value.
-# Returns the encrypted text.
 def caesar_cypher(text, delta):
     result = ''
     for character in text:
-        character_code = ord(character)
-        ok = False
-        for character_set in character_sets:
-            ok, new_character = caesar_cypher_character(character_code, character_set, delta)
-            if ok:
-                result += new_character
-                break
-        if not ok:
+        if character.isalpha():
+            base = ord('a')
+            if character == character.upper():
+                base = ord('A')
+            position = ord(character) - base
+            new_position = (position - delta) % 26
+            result += chr(base + new_position)
+        else:
             result += character
     return result
 
 
 # Unit test to make sure the function works correctly.
-def unit_test_caesar_cypher(text, expected, delta):
-    print("-----------------------------")
+def test_caesar_cypher(text, expected, delta):
     result = caesar_cypher(text, delta)
     print(f"Original text:  {text}")
     print(f"Encrypted text: {result}")
     print(f"Expected text:  {expected}")
     print(f"Result matched the expected value: {result == expected}")
+    print("-----------------------------")
 
 
 def part_1():
+    print(
+        '*************************************************\n Part 1 \n*************************************************')
     text = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG'
     expected = 'QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD'
-
     delta = 3
-    unit_test_caesar_cypher(text.upper(), expected.upper(), delta)
-    unit_test_caesar_cypher(text.lower(), expected.lower(), delta)
-    unit_test_caesar_cypher(text.title(), expected.title(), delta)
+    test_caesar_cypher(text.upper(), expected.upper(), delta)
+    test_caesar_cypher(text.lower(), expected.lower(), delta)
+    test_caesar_cypher(text.title(), expected.title(), delta)
 
 
 part_1()
@@ -61,4 +41,55 @@ part_1()
 
 # Part 2) Binary Search Algorithm Implementation in Python
 
+def binary_search(sorted_list, value):
+    if not sorted_list:
+        return False
+    else:
+        size = len(sorted_list)
+        guess = size // 2
+        if value == sorted_list[guess]:
+            return True
+        elif value > sorted_list[guess]:
+            return binary_search(sorted_list[guess + 1:], value)
+        else:
+            return binary_search(sorted_list[:guess], value)
+
+
+def part_2():
+    print(
+        '*************************************************\n Part 2 \n*************************************************')
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    print("Full list of prime numbers: ", primes)
+    for item in [2, 97, 25, 5]:
+        exists = binary_search(primes, item)
+        print(f"Looking for the value {item}, Exists: {exists}")
+
+
+part_2()
+
 # Part 3) Formula Implementation
+
+from math import factorial, sqrt
+
+
+def estimate_pi():
+    accumulated_sum = 0
+    limit_reached = False
+    k = 0
+    while not limit_reached:
+        term = factorial(4 * k) * (1103 + 26390 * k) / ((factorial(k) ** 4) * (396 ** (4 * k)))
+        accumulated_sum += term
+        limit_reached = term < 1e-15
+        k += 1
+    return 2 * sqrt(2) * accumulated_sum / 9801
+
+
+def part_3():
+    print(
+        '*************************************************\n Part 3 \n*************************************************')
+    reverse_of_pi = estimate_pi()
+    print(f"The result of the calculation is: {reverse_of_pi}")
+    print(f"Therefore the estimation of Pi is: {1 / reverse_of_pi}")
+
+
+part_3()
